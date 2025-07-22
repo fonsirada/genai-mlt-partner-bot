43a97fd5424a36dc12eeea01528349d4045433e0
fonsirada <fonsirada13@gmail.com>
2025-07-21 23:52:23 -0400

    added documentation

--- cik_module/cik_module.py
+++ cik_module/cik_module.py
@@ -1,6 +1,18 @@
+# Author: Alfonso Rada
+# Date: 07/21/2025
+# File Name: cik_module.py
+# Description:  A module that can be used to lookup the CIK number of companys registered with the SEC
+
 import requests
 
+'''
+    Given a URL to an SEC public JSON file, extract its information and use it to
+    lookup a company's CIK number by name or by ticker symbol.
+'''
 class SecEdgar:
+    '''
+    Grabbing the JSON from the SEC URL and filling dictionaries for lookup
+    '''
     def __init__(self, fileurl):
         self.fileurl = fileurl
 
@@ -10,6 +22,10 @@ class SecEdgar:
 
         self.cik_json_to_dict()
 
+    '''
+    Fills name_dict and ticker_dict dictionaries using data from 
+    SEC JSON file with company information
+    '''
     def cik_json_to_dict(self):
         self.name_dict = {}
         self.ticker_dict = {}
@@ -21,13 +37,19 @@ class SecEdgar:
             self.name_dict[name] = cik
             self.ticker_dict[ticker] = cik
 
+    '''
+    Given a company's name, return the corresponding CIK number, if it exists.
+    '''
     def name_to_cik(self, name):
         try:
             cik = self.name_dict[name]
             return (f"Name: {name}, CIK: {cik}")
         except KeyError:
             print(f"Name: {name}, does not exist.")
-    
+
+    '''
+    Given a company's ticker symbol, return the corresponding CIK number, if it exists.
+    '''
     def ticker_to_cik(self, ticker):
         try:
             cik = self.ticker_dict[ticker]
@@ -36,4 +58,3 @@ class SecEdgar:
             print(f"Ticker: {ticker}, does not exist.")
 
 sec = SecEdgar("https://www.sec.gov/files/company_tickers.json")
-print(sec.ticker_to_cik("NVDA"))
